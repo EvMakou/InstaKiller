522028be78c1eab716a0acbe7b429c249daffc6f
EvMakou <eugeniy.chumakov@gmail.com>
2024-03-09 14:04:13 +0300

    first commit

---
+++ InstaKiller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaKiller
+//
+//  Created by Yauheni Chumakou on 9.03.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
